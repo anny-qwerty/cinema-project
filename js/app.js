811e73db8c578e9bb3de797d617bfc2c527bab6a
anny-qwerty <92170019+anny-qwerty@users.noreply.github.com>
2021-10-10 19:04:44 +0300

    Add files via upload
    Changes for 10-10-2021

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('Hello');
+const schemeSvg = document.querySelector('.scheme-svg');
+const totalPriceTag = document.querySelector('.price-total');
+let costOfTicket = 800;
+let totalPrice = 0;
+
+schemeSvg.addEventListener('click', (event)=>{
+    // console.log(event.target);
+    // event.target.classList.toggle('active');
+    if(!event.target.classList.contains('booked') && !event.target.classList.contains('active')){
+        // console.log('Это место НЕ занято');
+        event.target.classList.add('active');
+        totalPrice += costOfTicket;
+        // console.log(totalPrice);
+        totalPriceTag.textContent = totalPrice;
+    }
+    else if(event.target.classList.contains('active') && (totalPrice != 0)){
+        event.target.classList.remove('active');
+        totalPrice -= costOfTicket;
+        // console.log(totalPrice);
+        totalPriceTag.textContent = totalPrice;
+    }
+})
